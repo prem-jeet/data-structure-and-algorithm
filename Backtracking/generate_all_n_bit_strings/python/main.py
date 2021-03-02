fb28fa7efdc0a9b2e0fd61925d2f5158cf01f524
prem-jeet <prem4300795@gmail.com>
2021-03-03 00:05:02 +0530

    binary string permutation

---
+++ Backtracking/generate_all_n_bit_strings/python/main.py
@@ -0,0 +1,13 @@
+from solution import Solution
+
+
+s = Solution()
+
+print("--- 1 bit string --")
+s.generate_string(2)
+
+print("\n--- 3 bit string --")
+s.generate_string(3)
+
+print("\n--- 4 bit string --")
+s.generate_string(4)
