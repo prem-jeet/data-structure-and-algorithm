76c7dce92a9b60a0cc18e1f3e982fb72ea64f369
prem-jeet <prem4300795@gmail.com>
2021-04-24 12:24:51 +0530

    stack using array in js

---
+++ Data structure/Stack/stack_using_array/js/main.js
@@ -0,0 +1,63 @@
+const { Stack } = require("./stack");
+
+const readline = require("readline-sync");
+
+function interface() {
+  console.log("------------------");
+  console.log("    1. Push       ");
+  console.log("    2. Pop        ");
+  console.log("    3. Peek       ");
+  console.log("    4. Size       ");
+  console.log("    5. Is Empty   ");
+  console.log("    6. Print      ");
+  console.log("    7. Exit       ");
+  console.log("------------------");
+  console.log("");
+}
+
+const s = new Stack();
+
+while (true) {
+  interface();
+  let choice = readline.questionInt("Enter your choice : ");
+
+  switch (choice) {
+    case 1: //Push
+      {
+        let data = readline.questionInt("Enter data : ");
+        s.push(data);
+      }
+      break;
+
+    case 2: // Pop
+      {
+        let data = s.pop();
+        console.log(data, " was popped from the stack.");
+      }
+      break;
+
+    case 3: // Peek
+      {
+        let data = s.peek();
+        console.log("Top the stack contains ", data);
+      }
+      break;
+
+    case 4: // size
+      console.log("stack contains ", s.size(), " items.");
+      break;
+
+    case 5: // Empty
+      console.log(s.empty());
+      break;
+
+    case 6: // Print
+      s.print();
+      break;
+    case 7: // exit
+      process.exit();
+
+    default:
+      console.log("Invalid choice");
+  }
+}
---
+++ Data structure/Stack/stack_using_array/js/stack.js
@@ -0,0 +1,67 @@
+class Stack {
+  constructor(n) {
+    this.arr = new Array(n).fill(0);
+    this.top = -1;
+
+    this.capacity = n;
+  }
+
+  // Push inserts the item at end
+  push(data) {
+    if (this.isFull()) {
+      console.log("Stack full.");
+      return;
+    }
+
+    this.arr[++this.top] = data;
+  }
+
+  // Pop funstion removes the item from end and return it
+  pop() {
+    if (this.isEmpty()) {
+      console.log("Stack Empty.");
+      return;
+    }
+    let temp = this.arr[this.top--];
+    return temp;
+  }
+
+  // Peek funtion gives the last item without removing it
+  peek() {
+    if (this.isEmpty()) {
+      console.log("Stack Empty.");
+      return;
+    }
+    let temp = this.arr[this.top];
+    return temp;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("Stack is empty");
+      return;
+    }
+
+    for (let i = this.top; i > -1; i--) console.log(this.arr[i]);
+  }
+
+  size() {
+    return this.top + 1;
+  }
+
+  empty() {
+    return this.isEmpty();
+  }
+  //Helper functions
+  isEmpty() {
+    return this.top == -1;
+  }
+
+  isFull() {
+    return this.size == this.capacity;
+  }
+}
+
+module.exports = {
+  Stack,
+};
